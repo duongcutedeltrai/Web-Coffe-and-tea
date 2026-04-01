64d233ce4f56a1dc1ac779109673cb41c64ac310
acer <duonghaiqb311@gmail.com>
2026-04-01 16:09:09 +0700

    dasdsad

---
+++ src/types/express-session.d.ts
@@ -0,0 +1,18 @@
+import "express-session";
+
+declare module "express-session" {
+    interface SessionData {
+        paymentData?: {
+            order_id: string,
+            user_id: string;
+            promotion_id: string;
+            totalPrice: number;
+            receiver_name: string;
+            receiver_phone: string;
+            delivery_address: string;
+            note?: string;
+            order_type: "DELIVERY";
+            payment_method: "vnpay"
+        };
+    }
+}
